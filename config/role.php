3e920fcc5fc559e7d04c8857d47eccdbe511ae6b
Jasmeen Virani <jasmeen.virani@gmail.com>
2023-08-02 16:19:21 +0530

    rename branch name

---
+++ config/role.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'General' => [
+        'users' => [
+            'users.list',
+            'users.create',
+            'users.update',
+            'users.view',
+            'users.destroy',
+            'users.auto_login',
+        ],
+        'profile' => [
+            'user_profiles.view',
+        ],
+        'roles' => [
+            'roles.create',
+            'roles.view',
+            'roles.update',
+            'roles.delete',
+        ],
+        'setting' => [
+            'setting.view',
+            'setting.update',
+        ],
+    ],
+
+];
+
+
+
