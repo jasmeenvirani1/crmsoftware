7ea52f5f349f242459074247a331464dcdedf6f3
Jasmeen Virani <jasmeen.virani@gmail.com>
2023-08-31 11:39:22 +0530

    some changes

--- resources/views/admin/catalog/pdf_template.blade.php
+++ resources/views/admin/catalog/pdf_template.blade.php
@@ -251,6 +251,14 @@
             </div>
         @endforeach
     </div>
+      <!-- Page2 End -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+      <script>
+          $(document).ready(function() {
+              window.print();
+          });
+      </script>
 </body>
 
 </html>
